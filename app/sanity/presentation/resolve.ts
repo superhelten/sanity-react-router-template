ba0a845aebf6481244d0de91df73a5f4f0204ef9
SimeonGriggs <simeon@hey.com>
2024-11-28 15:25:02 +0000

    extract location resolver

---
+++ app/sanity/presentation/resolve.ts
@@ -0,0 +1,21 @@
+import {defineLocations, PresentationPluginOptions} from 'sanity/presentation'
+
+export const resolve: PresentationPluginOptions['resolve'] = {
+  locations: {
+    record: defineLocations({
+      select: {
+        title: 'title',
+        slug: 'slug.current',
+      },
+      resolve: (doc) => ({
+        locations: [
+          {
+            title: doc?.title || 'Untitled',
+            href: `/records/${doc?.slug}`,
+          },
+          {title: 'Home', href: `/`},
+        ],
+      }),
+    }),
+  },
+}
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,10 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {defineLocations, presentationTool} from 'sanity/presentation'
+import {presentationTool} from 'sanity/presentation'
 import {structureTool} from 'sanity/structure'
 
 import {STUDIO_BASEPATH} from '~/sanity/constants'
+import {resolve} from '~/sanity/presentation/resolve'
 import {projectDetails} from '~/sanity/projectDetails'
 import schema from '~/sanity/schemaTypes'
 import {defaultDocumentNode, structure} from '~/sanity/structure'
@@ -15,28 +16,10 @@ export default defineConfig({
   plugins: [
     structureTool({structure, defaultDocumentNode}),
     presentationTool({
+      resolve,
       previewUrl: {
         previewMode: {
           enable: '/resource/preview',
-        },
-      },
-      resolve: {
-        locations: {
-          record: defineLocations({
-            select: {
-              title: 'title',
-              slug: 'slug.current',
-            },
-            resolve: (doc) => ({
-              locations: [
-                {
-                  title: doc?.title || 'Untitled',
-                  href: `/records/${doc?.slug}`,
-                },
-                {title: 'Home', href: `/`},
-              ],
-            }),
-          }),
         },
       },
     }),
